6f86fb19ac05e69643825d8e8eba7ee12fba4d14
enoraso <63040753+enoraso@users.noreply.github.com>
2024-03-25 01:19:25 +0200

    Added additional stats to enemy and updated TakeDamage function
    added armor resistance, attack type attribute and weakneses / resistances attributes

--- AdventureGame/EnemyClasses/Enemy.cs
+++ AdventureGame/EnemyClasses/Enemy.cs
@@ -7,8 +7,11 @@ class Enemy
     public int speed;
     public int experience;
     public int gold;
+    public List<string> weaknesses; // { "fire", "lightning" }
+    public List<string> resistances; // { "none" }
+    public string attackType;
 
-    public Enemy(string _name, int _health, int _attack, int _defense, int _speed, int _experience, int _gold)
+    public Enemy(string _name, int _health, int _attack, int _defense, int _speed, int _experience, int _gold, List<string> _weaknesses, List<string> _resistances, string _attackType)
     {
         name = _name;
         health = _health;
@@ -17,19 +20,41 @@ class Enemy
         speed = _speed;
         experience = _experience;
         gold = _gold;
+        weaknesses = _weaknesses;
+        resistances = _resistances;
+        attackType = _attackType;
     }
 
-    public void TakeDamage(int damage)
+    public void TakeDamage(int damage, string attackType)
     {
+        if (weaknesses.Contains(attackType))
+        {
+        // Apply double damage if weak to the attack type
+            damage *= 2;
+        }
+        // Check if the enemy has a resistance to the attack type
+        else if (resistances.Contains(attackType))
+        {
+        // Apply half damage if resistant to the attack type
+            damage /= 2;
+        }
+        damage -= defense;
+        if (damage < 0)
+        {
+        damage = 0;
+        }
         health -= damage;
         if (health < 0)
         {
             health = 0;
         }
+        Console.WriteLine(name" takes " + damage + " damage! Health left: " + this.health);
     }
-
-    public int DealDamage()
+    // can pass attack type with specific attacks if enemy can have several attack types.
+    public int DealDamage() 
     {
         return attack;
     }
+    //TODO
+    //public Item DropItem()
 }
